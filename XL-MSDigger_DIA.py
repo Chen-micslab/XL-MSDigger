ffb700fe2ed5f42f2dac65bbb483c13188a749d4
Chen-micslab <79675347+Chen-micslab@users.noreply.github.com>
2025-02-24 23:30:25 -0600

    Update XL-MSDigger_DIA.py

--- XL-MSDigger_DIA.py
+++ XL-MSDigger_DIA.py
@@ -15,7 +15,7 @@ def run():
     feature_det = feature_detect()
     diann_feature = feature_det.total_process(args.diann_report, args.DIA_library)
     diann_feature_dir = args.diann_report.split('.tsv')[0] + '_feature.csv'
-    diann_feature.to_csv(diann_feature_dir, inde)
+    diann_feature.to_csv(diann_feature_dir, index = False)
     rescore = dnn_rescore()
     rescore_results, rescore_test_results = rescore.run(diann_feature_dir)
 
